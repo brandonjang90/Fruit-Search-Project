fd8b623c5ae682bb198df8a049b80ea649650b05
Omar Qaddoumi <omarqaddoumi@gmail.com>
2023-11-07 03:05:51 +0000

    general cleanup

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+const suggestionsList = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-const search = (inputValue) => fruit.filter(f => f.toLowerCase().includes(inputValue));
+function search(inputValue) {
+	const inputLowerCase = inputValue.toLowerCase();
 
-function searchHandler(e) {
-  const inputVal = e.target.value;
-
-  // Only show suggestions if there is input
-  // this will ignore all space
-  if (inputVal.trim() !== '') {
-    const results = search(inputVal);
-    showSuggestions(results);
-  } else {
-    suggestions.textContent = '';
-  }
+	// Filter fruits that include the input string (case-insensitive)
+	return fruit.filter(f => f.toLowerCase().includes(inputLowerCase));
 }
 
-function showSuggestions(results, inputVal) {
-	suggestions.innerText = '';
+function clearSuggestions() {
+	suggestionsList.textContent = '';
+}
 
-	// If value is inputted, iterate results and for each fruit that matches user input, make a list element.
+function searchHandler(e) {
+	const inputVal = e.target.value;
 
-	if (inputVal !== '') {
-		results.forEach(result => {
-			let dropDown = document.createElement("li");
-			dropDown.textContent = result;
-			suggestions.appendChild(dropDown);
-		});
-	};
+	// Only show suggestions if there is input
+	if (inputVal.trim() !== '') {
+		const results = search(inputVal);
+		showSuggestions(results);
+	} else {
+		clearSuggestions()
+	}
+}
+
+function showSuggestions(results) {
+	clearSuggestions();
+
+	results.forEach(result => {
+		let listItem = document.createElement("li");
+		listItem.textContent = result;
+		suggestionsList.appendChild(listItem);
+	});
 }
 
 function useSuggestion(e) {
-	// makes sure that suggestions list is cleared once a suggestion is selected
-	suggestions.innerText = "";
-
-	// Set input value to selected suggestion
-	input.value = e.target.innerText;
+	if (e.target.tagName.toLowerCase() === 'li') {
+		input.value = e.target.textContent;
+		clearSuggestions(); // clear suggestions
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+suggestionsList.addEventListener('click', useSuggestion);
+
+
